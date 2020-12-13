1a51cd8ef024aceeac7af0552c9d7abcb428a3a8
Felipe Oliveira <felipephito@gmail.com>
2020-12-13 12:35:33 -0300

    Atualizado layout do arquivo catálogo.html

---
+++ js/jquery.js
@@ -0,0 +1,15 @@
+// SLIDER
+
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: false,
+        autoplay: true,
+        speed: 3000,
+        cssEase: 'ease',
+        dots: false,
+        fade: true,
+        infinite: true,
+        mobileFirst: true,
+        pauseOnHover: false,
+    });
+});
